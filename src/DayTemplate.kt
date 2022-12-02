e9de2dfe503ef52931c231e8d0d24b00321cb47c
Alexander Fredheim <alfr@netlight.com>
2022-12-02 17:45:04 +0100

    general template for solving tasks added

---
+++ src/DayTemplate.kt
@@ -0,0 +1,13 @@
+fun main() {
+    fun part1(comingSoon: Any): Int {
+        TODO()
+    }
+
+    fun part2(comingSoon: Any): Int {
+        TODO()
+    }
+
+    val input = readInput("")
+    println(part1(input))
+    println(part2(input))
+}
