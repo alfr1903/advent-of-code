06ae3abc3c1b72940cf6ed6c93114fa69ee97566
Alexander Fredheim <alfr@netlight.com>
2022-12-02 17:02:00 +0100

    day 1 finished V1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,27 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(calInput: List<String>): Int {
+        return aggregateCalories(calInput).maxBy { it.value }.value
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(calInput: List<String>): Int {
+        return aggregateCalories(calInput).values.sortedDescending().take(3).sum()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val input = readInput("Day01Input")
     println(part1(input))
     println(part2(input))
+}
+
+fun aggregateCalories(caloriesList: List<String>): MutableMap<Int, Int> {
+    val calMap: MutableMap<Int, Int> = mutableMapOf()
+    var index = 0
+    for (partCals in caloriesList) {
+        if (partCals.isNullOrBlank()){
+            index++
+        } else {
+            if (calMap.containsKey(index)) calMap[index] = calMap[index]!! + partCals.toInt()
+            else calMap[index] = partCals.toInt()
+        }
+    }
+    return calMap
 }
