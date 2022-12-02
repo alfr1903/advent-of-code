f73356f2bfcdaac70cd369a699d713a189fb2217
Alexander Fredheim <alfr@netlight.com>
2022-12-02 18:47:00 +0100

    day 2 - final try without looking at any other code

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,13 +1,37 @@
+const val OPPONENT_ROCK = "A"; const val OPPONENT_PAPER = "B"; const val OPPONENT_SCISSOR = "C"
+const val ROCK = "Y"; const val PAPER = "X"; const val SCISSOR = "Z"
+
+enum class HAND(val score: Int) {
+    A(1),
+    B(2),
+    C(3),
+    Y(1),
+    X(2),
+    Z(3),
+}
+
+private fun roundScore(roundString: String): Int {
+    val roundHands = Pair(roundString.substringBefore(' ').trim(), roundString.substringAfter(' '))
+    val theirHand = roundHands.first; val yourHand = roundHands.second
+    return HAND.valueOf(yourHand).score + outcome(theirHand, yourHand)
+}
+
+private fun outcome(theirHand: String, yourHand: String): Int {
+    if (theirHand == OPPONENT_ROCK && yourHand == SCISSOR) return 0
+    val score = HAND.valueOf(yourHand).score - HAND.valueOf(theirHand).score
+    return if (score < 0) 0 else if (score == 0) 3 else 6
+}
+
 fun main() {
-    fun part1(comingSoon: Any): Int {
+    fun part1(input: List<String>): Int {
+        return input.sumOf { roundScore(it) }
+    }
+
+    fun part2(input: List<String>): Int {
         TODO()
     }
 
-    fun part2(comingSoon: Any): Int {
-        TODO()
-    }
-
-    val input = readInput("")
+    val input = readInputAsList("Day02Input")
     println(part1(input))
     println(part2(input))
 }
