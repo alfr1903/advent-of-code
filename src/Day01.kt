8ad12b6f3ee7c50b755c399046ca67288d249dba
Alexander Fredheim <alfr@netlight.com>
2022-12-02 17:37:47 +0100

    day 1 finished, improved version

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,27 +1,21 @@
+import java.io.File
+
 fun main() {
-    fun part1(calInput: List<String>): Int {
-        return aggregateCalories(calInput).maxBy { it.value }.value
+    fun part1(calInput: String): Int {
+        val calorieChunks = calInput.split("\n\n").map { calPartial ->
+            calPartial.lines().sumOf { it.toInt() }
+        }
+        return calorieChunks.max()
     }
 
-    fun part2(calInput: List<String>): Int {
-        return aggregateCalories(calInput).values.sortedDescending().take(3).sum()
+    fun part2(calInput: String): Int {
+        val calorieChunks = calInput.split("\n\n").map { calPartial ->
+            calPartial.lines().sumOf { it.toInt() }
+        }
+        return calorieChunks.sorted().takeLast(3).sum()
     }
 
-    val input = readInput("Day01Input")
+    val input = File("src/Day01Input.txt").readText()
     println(part1(input))
     println(part2(input))
-}
-
-fun aggregateCalories(caloriesList: List<String>): MutableMap<Int, Int> {
-    val calMap: MutableMap<Int, Int> = mutableMapOf()
-    var index = 0
-    for (partCals in caloriesList) {
-        if (partCals.isNullOrBlank()){
-            index++
-        } else {
-            if (calMap.containsKey(index)) calMap[index] = calMap[index]!! + partCals.toInt()
-            else calMap[index] = partCals.toInt()
-        }
-    }
-    return calMap
 }
